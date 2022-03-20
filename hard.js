8ee830aefc7a00af2cb7d046f7e3e4b2b422eb07
merior nordway <meriornordway@yandex.ru>
2022-03-20 20:34:03 +0300

    erok 4

--- hard.js
+++ hard.js
@@ -19,39 +19,58 @@
 // strDegree = String(degree)
 // console.log(strDegree[0], strDegree[1])
 
-let lang = confirm('Язык русский или нет?');
+// let lang = confirm('Язык русский или нет?');
+// let namePerson = prompt('введите имя');
 
-console.log(lang)
+// console.log(lang)
 
-if (lang == true){
-  console.log('понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
-}
-else{
-  console.log('monday, tuesday, wednesday, thursday, friday, saturday, sunday');
-}
+// if (lang == true){
+//   console.log('понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
+// }
+// else{
+//   console.log('monday, tuesday, wednesday, thursday, friday, saturday, sunday');
+// }
 
-switch (lang) {
-  case true:
-    console.log('понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
-    break;
-  case false:
-    console.log('monday, tuesday, wednesday, thursday, friday, saturday, sunday');
-    break;
-  default:
-    console.log('это как? язык надо выбрать');
-}
+// switch (lang) {
+//   case true:
+//     console.log('понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
+//     break;
+//   case false:
+//     console.log('monday, tuesday, wednesday, thursday, friday, saturday, sunday');
+//     break;
+//   default:
+//     console.log('это как? язык надо выбрать');
+// }
 
-lang == true ? console.log('понедельник, вторник, среда, четверг, пятница, суббота, воскресенье') : console.log('monday, tuesday, wednesday, thursday, friday, saturday, sunday');
+// lang == true ? console.log('понедельник, вторник, среда, четверг, пятница, суббота, воскресенье') : console.log('monday, tuesday, wednesday, thursday, friday, saturday, sunday');
 
 
-//задание с именамт
+// //задание с именамт
 
-let namePerson = prompt('введите имя');
 
-console.log(namePerson)
+// console.log(namePerson)
 
-namePerson == 'артем' ? console.log('директор') : 
-namePerson == 'Артем' ? console.log('директор') : 
-namePerson == 'александр' ? console.log('преподаватель') : 
-namePerson == 'Александр' ? console.log('преподаватель') : 
-  console.log('студент');
+// namePerson == 'артем' ? console.log('директор') : 
+// namePerson == 'Артем' ? console.log('директор') : 
+// namePerson == 'александр' ? console.log('преподаватель') : 
+// namePerson == 'Александр' ? console.log('преподаватель') : 
+//   console.log('студент');
+
+let txt = prompt('напишите что нибудь');
+let txtNumber = +prompt('напишите тут число это для проверки');
+
+const txtFunction = function(a) {
+    if (typeof a !== 'string') {
+        return 'это не строка';
+    } else if (typeof a === 'string' && a.length < 30) {
+        a = txt.trim();
+        console.log(a);
+    } else if (typeof a === 'string' && a.length > 30 ) {
+        a = txt.trim();
+        a = txt.slice(0, 30) + '...';
+        console.log(a);  
+    };
+};
+
+txtFunction(txt);
+txtFunction(txtNumber);

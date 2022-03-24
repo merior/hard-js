3d96cd41faa034d27d71002c314a16a184de8e29
merior nordway <meriornordway@yandex.ru>
2022-03-24 11:34:54 +0300

    lesson 05

--- hard.js
+++ hard.js
@@ -1,76 +1,26 @@
-// let num = 266219
-// num = String(num)
-// let numLength = (num.length)
-// let result = 1
-// let strDegree
-// let degree = 1
-// let arr = num.split("")
+'use strict'
 
-// for (let i = 0; i < numLength; i++)
-//   result *= Number(num[i])
+// 1
 
-// for (let t = 0; t < 3; t++)
-//   degree *= result
-  
+let arr = ['12345', '23412', '32312', '44561', '22344', '67888', '455'];
 
-// console.log(arr)
-// console.log(result)
-// console.log(degree)
-// strDegree = String(degree)
-// console.log(strDegree[0], strDegree[1])
+for (let i = 1; i < arr.length; i++) {
+    if (arr[i].startsWith('2') || arr[i].startsWith('4')) {
+        console.log(arr[i]);
+    } else {
+        console.log('Число не начинается с числа 2 или 4');
+    }
+}
 
-// let lang = confirm('Язык русский или нет?');
-// let namePerson = prompt('введите имя');
-
-// console.log(lang)
-
-// if (lang == true){
-//   console.log('понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
-// }
-// else{
-//   console.log('monday, tuesday, wednesday, thursday, friday, saturday, sunday');
-// }
-
-// switch (lang) {
-//   case true:
-//     console.log('понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
-//     break;
-//   case false:
-//     console.log('monday, tuesday, wednesday, thursday, friday, saturday, sunday');
-//     break;
-//   default:
-//     console.log('это как? язык надо выбрать');
-// }
-
-// lang == true ? console.log('понедельник, вторник, среда, четверг, пятница, суббота, воскресенье') : console.log('monday, tuesday, wednesday, thursday, friday, saturday, sunday');
+// 2
 
 
-// //задание с именамт
+nextPrime:
+for (let i = 2; i <= 100; i++) {
 
+  for (let j = 2; j < i; j++) {
+    if (i % j == 0) continue nextPrime;
+  }
 
-// console.log(namePerson)
-
-// namePerson == 'артем' ? console.log('директор') : 
-// namePerson == 'Артем' ? console.log('директор') : 
-// namePerson == 'александр' ? console.log('преподаватель') : 
-// namePerson == 'Александр' ? console.log('преподаватель') : 
-//   console.log('студент');
-
-let txt = prompt('напишите что нибудь');
-let txtNumber = +prompt('напишите тут число это для проверки');
-
-const txtFunction = function(a) {
-    if (typeof a !== 'string') {
-        return 'это не строка';
-    } else if (typeof a === 'string' && a.length < 30) {
-        a = txt.trim();
-        console.log(a);
-    } else if (typeof a === 'string' && a.length > 30 ) {
-        a = txt.trim();
-        a = txt.slice(0, 30) + '...';
-        console.log(a);  
-    };
-};
-
-txtFunction(txt);
-txtFunction(txtNumber);
+  console.log(i, '  - простое число, ', 'делится на 1 и на: ', i)
+}
